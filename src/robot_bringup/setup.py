e939a042634db96592818401dedf1c29b7dc7d4b
Emil Djupvik <emil@djupvik.dev>
2026-03-13 10:28:14 +0000

    create bringup package

---
+++ src/robot_bringup/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'robot_bringup'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='user',
+    maintainer_email='user@example.com',
+    description='Launch files for the learnros project',
+    license='Apache-2.0',
+    entry_points={
+        'console_scripts': [],
+    },
+)
